1537670743bffc71e378ccf979fe5dec1c2c5baf
Mario Campaz <mcampaz2020@gmail.com>
2023-01-22 12:37:24 -0800

    [Setup] Initial project setup

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h2>Let's Get Started!</h2>
+        <ExpenseItem></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+function ExpenseItem(){
+    return (
+    <div>
+        <h3>Expense Item!</h3>
+    </div>);
+}
+
+export default ExpenseItem;
