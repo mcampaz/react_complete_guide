6e91dced50d6906296cdf3b49194096eae1284c5
Mario Campaz <mcampaz@hotmail.com>
2023-01-31 08:10:39 -0800

    [Add]
    New Expense form

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -7,28 +7,7 @@ import Card from '../UI/Card.js';
 const ExpenseItem = (props) => {
     // trigger state change; redrawn component
     const [expenseTitle, setTitle] = useState(props.expenseTitle);
-//     * @version 16.8.0
-//     * @see https://reactjs.org/docs/hooks-reference.html#usestate
-//     */
-//    function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
-//    // convenience overload when first argument is omitted
-//    /**
-//     * Returns a stateful value, and a function to update it.
-//     *
-//     * @version 16.8.0
-//     * @see https://reactjs.org/docs/hooks-reference.html#usestate
-//     */
-//    function useState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
-//    /**
-//     * An alternative to `useState`.
-//     *
-//     * `useReducer` is usually preferable to `useState` when you have complex state logic that involves
-//     * multiple sub-values. It also lets you optimize performance for components that trigger deep
-//     * updates because you can pass `dispatch` down instead of callbacks.
-//     *
-//     * @version 16.8.0
-//     * @see https://reactjs.org/docs/hooks-reference.html#usereducer
-    
+    console.log("Expense drwan by react!")
     const clickHandler = () => {
         setTitle('Updated!');
         console.log(expenseTitle);
---
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,16 @@
+import {Form} from 'react'
+
+import Expenseform from 'ExpenseForm.js'
+import Card from '../UI/Card.js';
+
+const NewExpense = (props) => {
+   return (
+        <Card>
+            <Expenseform>
+
+            </Expenseform>
+        </Card>
+    )
+};
+
+export default NewExpense;
